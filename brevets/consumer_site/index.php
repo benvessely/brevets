a18a83ce895501d871611690541460b68a649a2f
Ben Vessely <benvessely@gmail.com>
2024-07-14 14:58:21 -0700

    Fixed bug for consumer program when database empty

--- brevets/consumer_site/index.php
+++ brevets/consumer_site/index.php
@@ -15,13 +15,11 @@
             $obj = json_decode($json);
             $times = $obj->times;
             $checkpoint = 1;
-            if (!empty($times)){
-                foreach ($times as $control_time) {
-                    $open_time = $control_time->open;
-                    $close_time = $control_time->close;
-                    echo "<li>For checkpoint $checkpoint, Open: $open_time, Close: $close_time</li>";
-                    $checkpoint++; 
-                }
+            foreach ($times as $control_time) {
+                $open_time = $control_time->open;
+                $close_time = $control_time->close;
+                echo "<li>For checkpoint $checkpoint, Open: $open_time, Close: $close_time</li>";
+                $checkpoint++; 
             }
             ?>
         </ul> 
@@ -34,12 +32,10 @@
             $obj = json_decode($json);
             $times = $obj->times;
             $checkpoint = 1;
-            if (!empty($times)){
-                foreach ($times as $control_time) {
-                    $open_time = $control_time->open;
-                    echo "<li>For checkpoint $checkpoint, Open: $open_time</li>";
-                    $checkpoint++; 
-                }
+            foreach ($times as $control_time) {
+                $open_time = $control_time->open;
+                echo "<li>For checkpoint $checkpoint, Open: $open_time</li>";
+                $checkpoint++; 
             }
             ?>
         </ul> 
@@ -65,44 +61,62 @@
         <ul> 
             <?php
             $csv = file_get_contents('http://brevets-and-api:5000/listAll/csv');
-            // echo "DEBUG: $csv<br>"; //DB 
-            $split_csv = explode('\n', $csv);
-            // print_r($split_csv); //DB
-            $split_csv[0] = substr($split_csv[0], 1);
-            $split_csv[1] = substr($split_csv[1], 0, -2);
-            foreach ($split_csv as $element) {
-                echo "$element<br>"; 
+            // Check if $csv is empty (has length 5 in this case)
+            if (strlen($csv) == 5) {
+                echo ""; 
+            } else {
+                // echo "\$csv = $csv"; //DB 
+                $split_csv = explode('\n', $csv);
+                // print_r($split_csv); //DB
+                $split_csv[0] = substr($split_csv[0], 1);
+                $split_csv[1] = substr($split_csv[1], 0, -2);
+                foreach ($split_csv as $element) {
+                    echo "$element<br>"; 
+                }
             }
             ?>
         </ul> 
 
 
-        <h3>First k Open Times in CSV format with k=2</h3> 
+        <h3>First k Open Times in CSV format with k=2:</h3> 
         <ul> 
             <?php
             $csv = file_get_contents('http://brevets-and-api:5000/listOpenOnly/csv?top=2');
-            // echo "DEBUG: $csv<br>"; //DB 
-            $split_csv = explode('\n', $csv);
-            // print_r($split_csv); //DB
-            $split_csv[0] = substr($split_csv[0], 1);
-            $split_csv[1] = substr($split_csv[1], 0, -2);
-            foreach ($split_csv as $element) {
-                echo "$element<br>"; 
+            // $length = strlen($csv); //DB 
+            // echo "DEBUG: strlen($csv) = $length <br>"; //DB 
+
+            // Check if $csv is empty (has length 5 in this case)
+            if (strlen($csv) == 5) {
+                echo ""; 
+            } else { 
+                $split_csv = explode('\n', $csv);
+                // print_r($split_csv); //DB
+                $split_csv[0] = substr($split_csv[0], 1);
+                $split_csv[1] = substr($split_csv[1], 0, -2);
+                foreach ($split_csv as $element) {
+                    echo "$element<br>"; 
+                }
             }
             ?>
         </ul> 
 
-        <h3>First k Close Times in CSV format with k=2</h3> 
+        <h3>First k Close Times in CSV format with k=2:</h3> 
         <ul> 
             <?php
             $csv = file_get_contents('http://brevets-and-api:5000/listCloseOnly/csv?top=2');
             // echo "DEBUG: $csv<br>"; //DB 
-            $split_csv = explode('\n', $csv);
-            // print_r($split_csv); //DB
-            $split_csv[0] = substr($split_csv[0], 1);
-            $split_csv[1] = substr($split_csv[1], 0, -2);
-            foreach ($split_csv as $element) {
-                echo "$element<br>"; 
+
+            // Check if $csv is empty (has length 5 in this case)
+            if (strlen($csv) == 5) {
+                echo ""; 
+            } else { 
+                $split_csv = explode('\n', $csv);
+                // print_r($split_csv); //DB
+                $split_csv[0] = substr($split_csv[0], 1);
+                $split_csv[1] = substr($split_csv[1], 0, -2);
+                foreach ($split_csv as $element) {
+                    echo "$element<br>"; 
+                }
             }
             ?>
         </ul> 
